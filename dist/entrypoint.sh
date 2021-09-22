5b6b349bc32205b393a84d9c7057399da97b3ec8
Andre Parodi <aparodi@bn-mobile.com>
2021-09-22 16:58:31 +0200

    chhanged to entrypoint

--- dist/entrypoint.sh
+++ dist/entrypoint.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 # some debug
 # find .
-git config -l --show-origin
-echo "$GITHUB_USERNAME"
-echo "$GITHUB_PERSONAL_ACCESS_TOKEN"
+# git config -l --show-origin
+# echo "$GITHUB_USERNAME"
+# echo "$GITHUB_PERSONAL_ACCESS_TOKEN"
 # git config --global url.”https://${GITHUB_USERNAME}:${GITHUB_PERSONAL_ACCESS_TOKEN}@github.com/".insteadOf “https://github.com/"
 
 #

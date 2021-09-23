12b9de2ec05bc8b82010530237d34cdb89697d28
Andre Parodi <aparodi@bn-mobile.com>
2021-09-23 09:27:20 +0200

    fix

--- dist/entrypoint.sh
+++ dist/entrypoint.sh
@@ -14,8 +14,16 @@ echo "======Adding insteadof"
 git config --global url."ssh://git@github.com/".insteadOf "https://github.com/"
 
 echo "======Trying https"
-GIT_TRACE=2 GIT_CURL_VERBOSE=2 GIT_TRACE_PERFORMANCE=2 GIT_TRACE_PACK_ACCESS=2 GIT_TRACE_PACKET=2 GIT_TRACE_PACKFILE=2 GIT_TRACE_SETUP=2 GIT_TRACE_SHALLOW=2 git ls-remote "https://github.com/bn-mobile/UP_GitVersioning.git"
+GIT_TRACE=2 git ls-remote "https://github.com/bn-mobile/UP_GitVersioning.git"
 
+echo "======= try git with ssh explicit"
+GIT_TRACE=2 git ls-remote "ssh://git@github.com/bn-mobile/UP_GitVersioning.git"
+
+echo "======== try git with git no protocol"
+GIT_TRACE=2 git ls-remote "git@github.com/bn-mobile/UP_GitVersioning.git"
+
+echo "try ssh"
+ssh -vvv -o "BatchMode yes" git@github.com who
 echo "======known hosts"
 cat /root/.ssh/known_hosts
 #
